38efd6b5c94785d27edddd155c10eaa873b29051
Md Kamrul Amin <ka@mortgage-magic.co.uk>
2024-07-29 16:41:15 +0600

    removed unused import

--- lib/view/components/comment/compact_comment_tile.dart
+++ lib/view/components/comment/compact_comment_tile.dart
@@ -3,6 +3,8 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:media_mingle/state/comments/models/comment.dart';
 import 'package:media_mingle/state/user_info/providers/user_info_model_provider.dart';
 import 'package:media_mingle/view/components/animations/small_error_animation_view.dart';
+import 'package:media_mingle/view/components/rich_text/base_text.dart';
+import 'package:media_mingle/view/components/rich_text/rich_text_widget.dart';
 import 'package:media_mingle/view/components/rich_two_parts_text.dart';
 
 class CompactCommentTile extends ConsumerWidget {
@@ -17,9 +19,17 @@ class CompactCommentTile extends ConsumerWidget {
     ));
 
     return userInfo.when(data: (data) {
-      return RichTwoPartsText(
-        leftPart: data.displayName,
-        rightPart: comment.comment,
+      return RichTextWidget(
+        texts: [
+          BaseText(
+              text: '${data.displayName} ',
+              style: const TextStyle(fontWeight: FontWeight.bold)),
+          BaseText(text: comment.comment)
+        ],
+        styleForAll: const TextStyle(
+          height: 1.5,
+          color: Colors.white70,
+        ),
       );
     }, error: (error, stackTrace) {
       return const SmallErrorAnimationView();
